58abecaf0f35a7efd31ace2311f0f0ed734cfed2
Preston Ong <prestonojb@gmail.com>
2022-07-21 19:50:11 +0800

    Added Bootstrap Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
 
 type Order = {
   id: number;
@@ -10,10 +17,12 @@ type Order = {
 
 type Bot = {
   id: number;
-  status: "BUSY" | "IDLE";
+  status: "BUSY" | "AVAILABLE";
   orderId?: number;
   processOrder?: ReturnType<typeof setTimeout>;
 };
+
+const TIME_TO_PROCESS_ORDER = 10000;
 
 const App = () => {
   const [orders, setOrders] = useState<Order[]>([]);
@@ -32,7 +41,7 @@ const App = () => {
   };
 
   const addBot = () => {
-    const newBot: Bot = { id: nextBotId, status: "IDLE" };
+    const newBot: Bot = { id: nextBotId, status: "AVAILABLE" };
     setNextBotId(nextBotId + 1);
     setBots((currentBots) => [...currentBots, newBot]);
   };
@@ -48,12 +57,19 @@ const App = () => {
   };
 
   useEffect(() => {
-    const idleBots = bots.filter((bot) => bot.status === "IDLE");
-    const unprocessedOrders = orders.filter(
-      (order) =>
-        order.status === "PENDING" &&
-        !bots.map((bot) => bot.orderId).includes(order.id),
-    );
+    const idleBots = bots.filter((bot) => bot.status === "AVAILABLE");
+    const unprocessedOrders = orders
+      .filter(
+        (order) =>
+          order.status === "PENDING" &&
+          !bots.map((bot) => bot.orderId).includes(order.id),
+      )
+      .sort((a, b) => {
+        if (a.type === b.type) {
+          return a.id - b.id;
+        }
+        return a.type === "VIP" ? -1 : 1;
+      });
 
     if (idleBots.length === 0) return;
     if (unprocessedOrders.length === 0) return;
@@ -62,7 +78,7 @@ const App = () => {
 
     for (let unprocessedOrder of unprocessedOrders) {
       for (let bot of botsCopy) {
-        if (bot.status === "IDLE") {
+        if (bot.status === "AVAILABLE") {
           bot.status = "BUSY";
           bot.orderId = unprocessedOrder.id;
           bot.processOrder = setTimeout(() => {
@@ -78,14 +94,14 @@ const App = () => {
                 return prevBot.id === bot.id
                   ? {
                       ...prevBot,
-                      status: "IDLE",
+                      status: "AVAILABLE",
                       orderId: undefined,
                       processOrder: undefined,
                     }
                   : prevBot;
               }),
             );
-          }, 1000);
+          }, TIME_TO_PROCESS_ORDER);
           break;
         }
       }
@@ -94,46 +110,72 @@ const App = () => {
   }, [orders, bots]);
 
   return (
-    <>
-      <h2>Pending Orders</h2>
-      {orders
-        .filter((order) => order.status === "PENDING")
-        .map((order) => (
-          <div>
-            {"Order " + order.id} {order.type === "VIP" && "(VIP)"}{" "}
-            {getOrderBot(order.id) && `(Bot ${getOrderBot(order.id)?.id})`}
+    <Container fluid className="p-5">
+      <Row>
+        <Col className="mb-2">
+          <h2>Pending Orders</h2>
+          <div className="p-4 bg-light">
+            {orders
+              .filter((order) => order.status === "PENDING")
+              .map((order, idx) => (
+                <div key={idx}>
+                  {"Order " + order.id} {order.type === "VIP" && "(VIP)"}{" "}
+                  {getOrderBot(order.id) &&
+                    `(Bot ${getOrderBot(order.id)?.id})`}
+                </div>
+              ))}
           </div>
-        ))}
+        </Col>
 
-      <h2>Complete Orders</h2>
-      {orders
-        .filter((order) => order.status === "COMPLETE")
-        .map((order) => (
-          <div>
-            {"Order " + order.id} {order.type === "VIP" && "(VIP)"}{" "}
-            {getOrderBot(order.id) && `(Bot ${getOrderBot(order.id)?.id})`}
+        <Col className="mb-2">
+          <h2>Complete Orders</h2>
+          <div className="p-4 bg-light">
+            {orders
+              .filter((order) => order.status === "COMPLETE")
+              .map((order, idx) => (
+                <div key={idx}>
+                  {"Order " + order.id} {order.type === "VIP" && "(VIP)"}{" "}
+                  {getOrderBot(order.id) &&
+                    `(Bot ${getOrderBot(order.id)?.id})`}
+                </div>
+              ))}
           </div>
-        ))}
+        </Col>
+      </Row>
 
-      <div>
-        <button onClick={() => addOrder("Normal")}>Add Normal Order</button>
-        <button onClick={() => addOrder("VIP")}>- Add VIP Order</button>
-      </div>
+      <Stack direction="horizontal" gap={2}>
+        <Button variant="primary" onClick={() => addOrder("Normal")}>
+          Add Normal Order
+        </Button>
+        <Button variant="secondary" onClick={() => addOrder("VIP")}>
+          Add VIP Order
+        </Button>
+      </Stack>
 
       <hr />
 
-      <h2>Idle Bots</h2>
-      {bots
-        .filter((bot) => bot.status === "IDLE")
-        .map((bot) => (
-          <div>{"Bot " + bot.id}</div>
-        ))}
+      <Row className="mb-3">
+        <Col>
+          <h2>Idle Bots</h2>
+          <div className="p-4 bg-light">
+            {bots
+              .filter((bot) => bot.status === "AVAILABLE")
+              .map((bot, idx) => (
+                <div key={idx}>{"Bot " + bot.id}</div>
+              ))}
+          </div>
+        </Col>
+      </Row>
 
-      <div>
-        <button onClick={addBot}>+ Bot</button>
-        <button onClick={removeBot}>- Bot</button>
-      </div>
-    </>
+      <Stack direction="horizontal" gap={2}>
+        <Button className="me-1" variant="primary" onClick={addBot}>
+          + Bot
+        </Button>
+        <Button variant="secondary" onClick={removeBot}>
+          - Bot
+        </Button>
+      </Stack>
+    </Container>
   );
 };
 
